3cd88d093b5921adcddaa745c6ab94c4ea6a0429
MohammadNasir520 <nasirahsan520@gmail.com>
2022-08-23 01:04:42 +0600

    alert added

--- js/player-buy.js
+++ js/player-buy.js
@@ -1,12 +1,47 @@
 const playerNameAray = [];
 
+
+
+
+function getSelectButton(element) {
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    if (playerNameAray.length < 5) {
+        playerNameAray.push(playerName)
+
+
+    }
+    else if (playerNameAray.length === 5) {
+        alert('you can not buy more than five players')
+        return;
+    }
+
+    const totalSelectedPlayer = document.getElementById('total-selected-player');
+    totalSelectedPlayer.innerText = playerNameAray.length;
+
+
+    const selectedNumberString = totalSelectedPlayer.innerText;
+    const selectedNumberStringParsInt = parseInt(selectedNumberString);
+    console.log(selectedNumberStringParsInt)
+
+
+
+
+
+
+    setPlayerName(playerName);
+}
+
 function setPlayerName(name) {
-    console.log(name)
+
+
     const tableBody = document.getElementById('name-place');
+
     tableBody.innerHTML = ''
+
     for (let i = 0; i < playerNameAray.length; i++) {
         const playersName = playerNameAray[i];
         const tr = document.createElement('tr');
+
         tr.innerHTML = `
         <td>${i + 1}</td>
         <td>${playersName}</td>
@@ -14,18 +49,5 @@ function setPlayerName(name) {
         tableBody.appendChild(tr)
     }
 
+
 }
-
-function getSelectButton(element) {
-    const playerName = element.parentNode.parentNode.children[0].innerText;
-
-    playerNameAray.push(playerName)
-
-
-    document.getElementById('total-selected-player').innerText = playerNameAray.length;
-
-    setPlayerName(playerName);
-}
-
-
-
